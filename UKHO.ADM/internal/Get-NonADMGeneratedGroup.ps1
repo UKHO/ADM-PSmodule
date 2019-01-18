354821083ed1e792b787c80dde7c4703c577a832
Luke Duddridge <luke.duddridge@ukho.gov.uk>
2019-01-18 12:35:57 +0000

    Only enter workflow for AD group if domain is part of distinguishedname

--- UKHO.ADM/internal/Get-NonADMGeneratedGroup.ps1
+++ UKHO.ADM/internal/Get-NonADMGeneratedGroup.ps1
@@ -15,13 +15,15 @@ function Get-NonADMGeneratedGroup {
     process {
         if ($null -ne $GroupConfig.Groups -and $GroupConfig.Groups.Count -gt 0) {
             $GroupConfig.Groups | ForEach-Object {
-                $groupGroup = [ADGroup]::new($_.DistinguishedName, $OU.Domain)
-                if (Check-GroupExists $groupGroup) {
-                    Write-Verbose "Adding Group $($groupGroup.DistinguishedName) TO GROUP $($ggGroup.DistinguishedName)"
-                    $ggGroup.ADGroupMembers += $groupGroup
-                }
-                else {
-                    Write-Verbose    "Group $($groupGroup.DistinguishedName) Not found in $($OU.Domain.DistinguishedName)"
+                if ($_.DistinguishedName -like "*$($OU.Domain.DistinguishedName)") {
+                    $groupGroup = [ADGroup]::new($_.DistinguishedName, $OU.Domain)
+                    if (Check-GroupExists $groupGroup) {
+                        Write-Verbose "Adding Group $($groupGroup.DistinguishedName) TO GROUP $($ggGroup.DistinguishedName)"
+                        $ggGroup.ADGroupMembers += $groupGroup
+                    }
+                    else {
+                        Write-Error "Group $($groupGroup.DistinguishedName) Not found in $($OU.Domain.DistinguishedName)"
+                    }
                 }
             }
         }
