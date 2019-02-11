8fa62387210d96bca1bae884ba8f2c21be3c35d6
Luke Duddridge <338863+elduddz@users.noreply.github.com>
2019-02-11 16:03:03 +0000

    fixed Write-Colour call by updating to 0.85 (#28)
    * increased version of PSWriteColor to 0.85
    
    * update to use Write-Colour

--- UKHO.ADM/Functions/Update-AD.ps1
+++ UKHO.ADM/Functions/Update-AD.ps1
@@ -6,29 +6,29 @@ function Update-AD {
     )
     begin {}
     process {
-        Write-Color -LinesBefore 1 "Actions are indicated with the following symbols:" -Color White
-        Write-Color -StartTab 1 "+", " adding or creating" -Color Green, White
-        Write-Color -StartTab 1 "-", " removing or deleting" -Color Red, White
-        Write-Color -StartTab 1 "~", " modification or change" -Color Yellow, White
-        Write-Color -StartTab 1 "x", " errors have occurred"  -Color Magenta, White
+        Write-Colour -LinesBefore 1 "Actions are indicated with the following symbols:" -Color White
+        Write-Colour -StartTab 1 "+", " adding or creating" -Color Green, White
+        Write-Colour -StartTab 1 "-", " removing or deleting" -Color Red, White
+        Write-Colour -StartTab 1 "~", " modification or change" -Color Yellow, White
+        Write-Colour -StartTab 1 "x", " errors have occurred"  -Color Magenta, White
 
         $out = Generate-ConfigurationObject -ConfigData $cd 
 
-        Write-Color -LinesBefore 1 "UKHO.ADM will perform the following changes:" -Color Blue
+        Write-Colour -LinesBefore 1 "UKHO.ADM will perform the following changes:" -Color Blue
 
         $ADChanges = Get-ADChanges($out)
 
-        Write-Color -LinesBefore 1 "Change Summary:" -Color Gray
-        Write-Color -StartTab 1 "Created OU: $($ADChanges.CreatedOUs)" -Color Gray
-        Write-Color -StartTab 1 "Created Groups: $($ADChanges.CreatedGroups)" -Color Gray
-        Write-Color -StartTab 1 "Added Groups: $($ADChanges.AddedGroups)" -Color Gray
-        Write-Color -StartTab 1 "Removed Groups: $($ADChanges.RemovedGroups)" -Color Gray
-        Write-Color -StartTab 1 "Removed Users: $($ADChanges.RemovedUsers)" -Color Gray
-        Write-Color -StartTab 1 "Added Users: $($ADChanges.AddedUsers)" -Color Gray
+        Write-Colour -LinesBefore 1 "Change Summary:" -Color Gray
+        Write-Colour -StartTab 1 "Created OU: $($ADChanges.CreatedOUs)" -Color Gray
+        Write-Colour -StartTab 1 "Created Groups: $($ADChanges.CreatedGroups)" -Color Gray
+        Write-Colour -StartTab 1 "Added Groups: $($ADChanges.AddedGroups)" -Color Gray
+        Write-Colour -StartTab 1 "Removed Groups: $($ADChanges.RemovedGroups)" -Color Gray
+        Write-Colour -StartTab 1 "Removed Users: $($ADChanges.RemovedUsers)" -Color Gray
+        Write-Colour -StartTab 1 "Added Users: $($ADChanges.AddedUsers)" -Color Gray
 
         if ($ApplyChanges) {
 
-            Write-Color -LinesBefore 4 "UKHO.ADM will now apply the changes" -Color Blue
+            Write-Colour -LinesBefore 4 "UKHO.ADM will now apply the changes" -Color Blue
 
             $ADChanges.ApplyChanges()        
         }
