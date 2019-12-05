06a2cf58b16104ab2a49339e64bcf787a11f6c50
Luke Duddridge <luke.duddridge@ukho.gov.uk>
2019-12-05 16:10:44 +0000

    update build actions

---
+++ Build/Build.ps1
@@ -0,0 +1,17 @@
+param($ManifestFilePath, $PreReleaseTag, $Branch)
+
+Import-Module $PSScriptRoot\..\UKHO.BuildAndDeploy
+
+# Install required modules
+$ModuleLocation = (Split-Path $ManifestFilePath)
+$ManifestFileName = (Split-Path $ManifestFilePath -Leaf)
+Install-RequiredModules $ModuleLocation $ManifestFileName
+
+# Update manifest to include all functions found within the `functions` folder
+if(Test-Path("$ModuleLocation\functions")){
+    $FunctionsToExport += Get-ChildItem "$ModuleLocation\functions" | Select -expand BaseName
+}
+Update-ModuleManifest -Path $ManifestFilePath -FunctionsToExport $FunctionsToExport
+
+# Set module on as pre-release if not on master branch
+Set-ModuleAsPreRelease -ManifestFilePath $ManifestFilePath -PreReleaseTag $PreReleaseTag -Branch $Branch
