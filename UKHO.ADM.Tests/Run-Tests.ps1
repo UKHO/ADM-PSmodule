fcb2429562de132d1f275d449281d0d0795c23bc
Luke Duddridge <luke.duddridge@ukho.gov.uk>
2019-12-06 09:08:20 +0000

    import required modules

--- UKHO.ADM.Tests/Run-Tests.ps1
+++ UKHO.ADM.Tests/Run-Tests.ps1
@@ -1,8 +1,14 @@
+param($ManifestFilePath)
 Set-Location $PSScriptRoot
 
 $mut = "UKHO.ADM"
 
 $moduleRoot = "$PSScriptRoot\..\$mut"
+
+# Install required modules
+$ModuleLocation = (Split-Path $ManifestFilePath)
+$ManifestFileName = (Split-Path $ManifestFilePath -Leaf)
+Install-RequiredModules $ModuleLocation $ManifestFileName
 
 Remove-Module $mut -ErrorAction SilentlyContinue -WarningAction SilentlyContinue
 Import-Module $moduleRoot -ErrorAction SilentlyContinue -WarningAction SilentlyContinue
