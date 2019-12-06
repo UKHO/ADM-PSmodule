a9d067e38584b98455a10a67e80cfeb1f3cfd2d6
Luke Duddridge <luke.duddridge@ukho.gov.uk>
2019-12-06 09:23:50 +0000

    remove reload of module

--- UKHO.ADM.Tests/Run-Tests.ps1
+++ UKHO.ADM.Tests/Run-Tests.ps1
@@ -1,14 +1,8 @@
-param($ManifestFilePath)
 Set-Location $PSScriptRoot
 
 $mut = "UKHO.ADM"
 
 $moduleRoot = "$PSScriptRoot\..\$mut"
-
-# Install required modules
-$ModuleLocation = (Split-Path $ManifestFilePath)
-$ManifestFileName = (Split-Path $ManifestFilePath -Leaf)
-Install-RequiredModules $ModuleLocation $ManifestFileName
 
 Remove-Module $mut -ErrorAction SilentlyContinue -WarningAction SilentlyContinue
 Import-Module $moduleRoot -ErrorAction SilentlyContinue -WarningAction SilentlyContinue
